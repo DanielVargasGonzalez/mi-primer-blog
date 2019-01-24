348d27a3042c3c54212b5796b993489c0d5674a8
daniel <danielavg87@hotmail.com>
2019-01-24 14:12:20 +0100

    acabada, a partir de aqui, estetico

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,7 +1,11 @@
-from django.urls import path, include
+from django.conf.urls import include, url
 from django.contrib import admin
 
+from django.contrib.auth import views
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('blog.urls')),
+    url(r'^admin/', admin.site.urls),
+    url(r'^accounts/login/$', views.LoginView.as_view(), name='login'),
+    url(r'^accounts/logout/$', views.LogoutView.as_view(), name='logout', kwargs={'next_page': '/'}),
+    url(r'', include('blog.urls')),
 ]
